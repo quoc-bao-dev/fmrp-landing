9bd5cc1a5c1c0cff6132c3fb08acc9bb84d18426
Cao Văn Quý <bckyo2000@gmail.com>
2025-04-22 15:54:35 +0700

    update deploy

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,12 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      "@typescript-eslint/no-explicit-any": "warn" // hoặc "error" nếu bạn muốn bắt buộc tránh any
-    }
-  }
+  ...compat.extends("next/core-web-vitals", "next/typescript", "@typescript-eslint/no-explicit-any", "@typescript-eslint/no-unused-vars"),
 ];
 
 export default eslintConfig;
