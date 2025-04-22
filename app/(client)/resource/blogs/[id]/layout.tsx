386b4260429b75a23e5a618a706f489b8dbb9958
Cao Văn Quý <bckyo2000@gmail.com>
2025-04-22 17:41:58 +0700

    update deploy temp

--- app/(client)/resource/blogs/[id]/layout.tsx
+++ app/(client)/resource/blogs/[id]/layout.tsx
@@ -4,25 +4,25 @@ import { GenerateMetadataFromData } from "@/components/seo/GenerateMetadataFromD
 import apiBlogs from "@/services/blogs/blogs.services";
 import { ReactNode } from "react";
 
-export async function generateMetadata({ params }: any) {
-    try {
-        const { id } = await params
+// export async function generateMetadata({ params }: any) {
+//     try {
+//         const { id } = await params
 
-        console.log('id', id);
+//         console.log('id', id);
 
-        const { data } = await apiBlogs.getDetailBlog(id);
+//         const { data } = await apiBlogs.getDetailBlog(id);
 
-        const canonicalUrl = `${process.env.NEXT_PUBLIC_URL_WEBSITE}/resource/blogs/${id}`;
+//         const canonicalUrl = `${process.env.NEXT_PUBLIC_URL_WEBSITE}/resource/blogs/${id}`;
 
-        return GenerateMetadataFromData(data?.data, canonicalUrl);
-    } catch (error) {
-        console.error('generateMetadata error:', error);
-        return {
-            title: 'Không tìm thấy',
-            description: 'Bài viết bạn yêu cầu không tồn tại.',
-        };
-    }
-}
+//         return GenerateMetadataFromData(data?.data, canonicalUrl);
+//     } catch (error) {
+//         console.error('generateMetadata error:', error);
+//         return {
+//             title: 'Không tìm thấy',
+//             description: 'Bài viết bạn yêu cầu không tồn tại.',
+//         };
+//     }
+// }
 
 export default function BlogDetailLayout({
     children,
