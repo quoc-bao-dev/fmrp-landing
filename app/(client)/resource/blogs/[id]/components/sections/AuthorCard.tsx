ab1eaf767e6f4211a7d7d818db9cce866e460688
Noa <nguyenduynoa198@gmail.com>
2025-06-09 14:46:30 +0700

    fix: add link

--- app/(client)/resource/blogs/[id]/components/sections/AuthorCard.tsx
+++ app/(client)/resource/blogs/[id]/components/sections/AuthorCard.tsx
@@ -2,11 +2,16 @@ import FacebookOriginIcon from "@/components/icons/social-media/FacebookOriginIc
 import LinkedOriginIcon from "@/components/icons/social-media/LinkedOriginIcon";
 import XOriginIcon from "@/components/icons/social-media/XOriginIcon";
 import Image from "next/image";
+import Link from "next/link";
 
 const AuthorCard = () => {
   return (
-    <div className="flex flex-col xl:flex-row gap-6 py-6 px-0 xl:px-8 2xl:px-12 rounded-3xl bg-white xl:shadow-[0px_1px_2px_0px_#1212170F,0px_1px_3px_0px_#1212171A]">
-      <div className="size-16 flex-shrink-0 hidden xl:block">
+    <div className="flex flex-col xl:flex-row gap-6 py-6 px-0 xl:px-6 2xl:px-12 rounded-3xl bg-white xl:shadow-[0px_1px_2px_0px_#1212170F,0px_1px_3px_0px_#1212171A]">
+      <Link
+        href="https://fososoft.com/blogs/an-bui-la-ai-hanh-trinh-kien-tao-giai-phap-cong-nghe-cho-doanh-nghiep-smes"
+        target="_blank"
+        className="size-16 flex-shrink-0 hidden xl:block"
+      >
         <Image
           src="/Author/AnBui.jpg"
           alt="author"
@@ -14,11 +19,15 @@ const AuthorCard = () => {
           height={500}
           className="rounded-full object-cover w-full h-full"
         />
-      </div>
+      </Link>
       <div className="flex flex-col gap-4 w-full">
         <div className="flex flex-col xl:flex-row justify-between items-start gap-4 w-full">
           <div className="flex gap-4 items-center">
-            <div className="size-16 flex-shrink-0 xl:hidden">
+            <Link
+              href="https://fososoft.com/blogs/an-bui-la-ai-hanh-trinh-kien-tao-giai-phap-cong-nghe-cho-doanh-nghiep-smes"
+              target="_blank"
+              className="size-16 flex-shrink-0 xl:hidden"
+            >
               <Image
                 src="/Author/AnBui.jpg"
                 alt="author"
@@ -26,9 +35,15 @@ const AuthorCard = () => {
                 height={500}
                 className="rounded-full object-cover w-full h-full"
               />
-            </div>
+            </Link>
             <div className="flex flex-col gap-1">
-              <h3 className="text-[#15AA7A] font-bold text-2xl xl:text-3xl">An Bùi</h3>
+              <Link
+                href="https://fososoft.com/blogs/an-bui-la-ai-hanh-trinh-kien-tao-giai-phap-cong-nghe-cho-doanh-nghiep-smes"
+                target="_blank"
+                className="text-[#15AA7A] font-bold text-2xl xl:text-3xl"
+              >
+                An Bùi
+              </Link>
               <p className="text-sm xl:text-base font-bold text-[#809FB8] uppercase">
                 chuyên gia công nghệ{" "}
               </p>
@@ -46,7 +61,7 @@ const AuthorCard = () => {
             </div>
           </div>
         </div>
-        <p className="text-sm xl:text-base font-medium text-[#231F20]">
+        <p className="text-sm xl:text-base font-medium text-[#231F20] text-justify">
           Nhà lãnh đạo xuất sắc đứng đầu FOSO, với hơn một thập kỷ kinh nghiệm,
           là chuyên gia trong lĩnh vực phần mềm quản lý sản xuất, thiết kế ứng
           dụng di động và phát triển website chuyên nghiệp. Anh không chỉ mang
